6dc8ad31fc344de8fb5cd79b96bc8de48ce0002d
MahdiMac <mahdijj0090@gmail.com>
2026-05-21 19:33:14 +0200

    project init

---
+++ src/main/java/com/daykal/typeblog/TypeblogApplication.java
@@ -0,0 +1,13 @@
+package com.daykal.typeblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypeblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypeblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daykal/typeblog/TypeblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daykal.typeblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypeblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
