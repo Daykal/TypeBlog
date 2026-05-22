c7f962f6b281bbfd7252a45dc9befcf14e622cae
MahdiMac <mahdijj0090@gmail.com>
2026-05-22 11:10:57 +0200

    add relations for Category, Post, Tag, User, and interfaces for them

---
+++ src/main/java/com/daykal/typeblog/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.daykal.typeblog.repositories;
+
+import com.daykal.typeblog.domain.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
+}
