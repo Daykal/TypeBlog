59a22b6fc21023fd3e027dc5cfafb77a11eecd58
MahdiMac <mahdijj0090@gmail.com>
2026-05-23 20:11:20 +0200

    add security config and bridged user with userDetails interface, handle unauth user error

---
+++ src/main/java/com/daykal/typeblog/security/BlogUserDetailsService.java
@@ -0,0 +1,20 @@
+package com.daykal.typeblog.security;
+
+import com.daykal.typeblog.domain.entities.User;
+import com.daykal.typeblog.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class BlogUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+        return new BlogUserDetails(user);
+    }
+}
