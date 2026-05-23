3b6f1d06692ee5df673eb4a426ab9f40c45d1ca9
MahdiMac <mahdijj0090@gmail.com>
2026-05-23 19:07:15 +0200

    add error controller handler

---
+++ src/main/java/com/daykal/typeblog/domain/dtos/ApiErrorResponse.java
@@ -0,0 +1,29 @@
+package com.daykal.typeblog.domain.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiErrorResponse {
+    private int status;
+    private String message;
+    private List<FieldError> error;
+
+
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class FieldError{
+        private int field;
+        private String message;
+    }
+}
